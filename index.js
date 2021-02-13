95cb4042cc84a86a747841e0e09840bdca610a7a
notDavidHenderson <sn2catt@gmail.com>
2021-02-13 01:43:05 +0000

    all finished boss

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+let names = ['david','josh', 'anna'];
+
+function writeCards(names,event){
+  for (var i = 0; i < names.length; i++) {
+    console.log(`thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+  }
+}
+writeCards(names,`birthday`);
+
+function countDown(int){
+  while (int >= 0 ) {
+    console.log(int);
+    int--;
+  }
+}
+countDown(10);
